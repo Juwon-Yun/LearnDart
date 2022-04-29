b946f2093a97ec135226e166abd22e1216bdf752
Juwon-Yun <eooll2@daum.net>
2022-04-29 13:52:45 +0900

    feat: while loop

--- bin/basic/forLoop.dart
+++ bin/basic/forLoop.dart
@@ -20,7 +20,18 @@ void main(){
     print("values : " + str);
   }
 
-  // int total =
-  //
-  // while()
+  int total = 0;
+
+  while( total  < 10){
+    total += 1;
+  }
+  print(total);
+
+  total = 0;
+  do{
+    total += 1;
+  }while( total < 10);
+
+  print(total);
+
 }
