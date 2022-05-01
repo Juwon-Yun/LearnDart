3d7f35d414cc89f2ee512b6eaecece930360baba
Juwon-Yun <eooll2@daum.net>
2022-05-01 16:17:47 +0900

    feat: stream version await => yield* :D

--- bin/async/streamFunc.dart
+++ bin/async/streamFunc.dart
@@ -1,12 +1,17 @@
 void main() async{
-  calculate(1).listen((event) {
-    print("calculate(1) : $event");
-  });
+  // calculate(1).listen((event) {
+  //   print("calculate(1) : $event");
+  // });
+  //
+  // calculate(2).listen((event) {
+  //   print("calculate(2) : $event");
+  // });
+  //
 
-  calculate(2).listen((event) {
-    print("calculate(2) : $event");
+  // stream에서도 await와 비슷한 기능을 사용할 수 있다.
+  playAllStream().listen((event) {
+    print(event);
   });
-
 }
 
 // Future + Stream
@@ -16,4 +21,10 @@ Stream<int> calculate(int number) async*{
 
     await Future.delayed(Duration(seconds: 1));
   }
+}
+
+// yield*은 해당 stream의 모든 값을 다 가져올 때 까지 기다림
+Stream<int> playAllStream() async*{
+  yield* calculate(1);
+  yield* calculate(1000);
 }
